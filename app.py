7760e68405becb40423137f4d12d24c43ac69e5a
Jordan <jalieb0812@gmail.com>
2020-04-26 18:00:52 -0400

    added newsest socket.io stuff

--- app.py
+++ app.py
@@ -355,14 +355,13 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-
+"""
 if __name__ == "__main__":
  port = int(os.environ.get("PORT", 8080))
  socketio.run(app, host="0.0.0.0", port=port)
-
 """
+
 if __name__ == '__main__':
 
 
     socketio.run(app)
-"""
